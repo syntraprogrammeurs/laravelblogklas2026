bd65cc2a4b4279c82e38b6b51b1a66ba8772f8b0
Tom Vanhoutte <syntraprogrammeurs@gmail.com>
2026-02-25 11:33:41 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+// Bootstrap setup (Laravel default)
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// // Alpine
+// import Alpine from 'alpinejs'
+// window.Alpine = Alpine
+// Alpine.start()
+
+// SB Admin core script
+import './sb-admin/scripts'
+
+// SB Admin plugins
+import './sb-admin/datatables-simple-demo'
+import './sb-admin/chart-area-demo'
+import './sb-admin/chart-bar-demo'
+import './sb-admin/chart-pie-demo'
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,23 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('frontend.home'))->name('frontend.home');
+//frontend routes
+
+Route::get('/', fn () => view('frontend.home'))->name('home');
 Route::get('/contact', fn () => view('frontend.contact'))->name('frontend.contact');
 
+//backend routes
 Route::get('/backend', function () {
     return view('backend.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+
 Route::middleware(['auth'])->group(function(){
     Route::resource('/backend/users', UserController::class);
 });
+
+
 
 
 require __DIR__.'/settings.php';
